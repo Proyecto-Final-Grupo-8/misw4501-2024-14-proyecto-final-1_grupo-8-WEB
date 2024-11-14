3dcb784ff9fe09f119a1f8ab8a113e28ee8b7b61
dcorread <d.corread@uniandes.edu.co>
2024-11-13 21:58:33 -0500

    feat: Buttons and bug was working on.

--- src/_nav.js
+++ src/_nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CIcon from '@coreui/icons-react';
 import { useTranslation } from 'react-i18next';
-import { cilUserPlus } from '@coreui/icons';
+import { cilUserPlus, cilMobile } from '@coreui/icons';
 import { CNavItem, CNavTitle } from '@coreui/react';
 
 const _nav = [
@@ -20,6 +20,12 @@ const _nav = [
     to: '/register',
     icon: <CIcon icon={cilUserPlus} customClassName="nav-icon" />,
   },
+  {
+    component: CNavItem,
+    name: 'Tableros',
+    to: '/',
+    icon: <CIcon icon={cilMobile} customClassName="nav-icon" />
+  }
 ];
 
 export default _nav;
